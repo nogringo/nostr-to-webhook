1a950316e4f2956128d4483863031641ee3756db
Russell <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-05-11 18:02:58 +0200

    first commit

---
+++ bin/nostr_to_webhook.dart
@@ -0,0 +1,5 @@
+import 'package:nostr_to_webhook/nostr_to_webhook.dart' as nostr_to_webhook;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nostr_to_webhook.calculate()}!');
+}
---
+++ test/nostr_to_webhook_test.dart
@@ -0,0 +1,8 @@
+import 'package:nostr_to_webhook/nostr_to_webhook.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
